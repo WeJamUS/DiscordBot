4b334c2b3574e6cc477dcc3b98de7b8d5e47b942
Daniel Sun <danielcsun@hotmail.com>
2021-08-18 20:10:56 -0700

    dadbot spacing detection hotfix

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ class MessageTypes(enum.Enum):
 
 your=['your', 'ur', 'yo', 'joe']
 mom=['mom', 'momma', 'mother', 'mum', 'mama']
-im = ["i\'m", "i am", "i’m", "ima", "im"]
+im = ["i\'m", "i am", "i’m", "im", "ima"]
 
 gb = load('gb.pkl')
 vectorizer = load('vectorizer.pkl')
@@ -53,11 +53,17 @@ async def on_message(message):
 
     try:
         for s in im:
-            idx = lowercaseMsg.rfind(s+" ")
+            idx = lowercaseMsg.rfind(s + " ")
+            if not idx == 0:
+                idx = lowercaseMsg.rfind(" " + s + " ")
+                if idx == -1:
+                    continue
+                idx += 1
             if idx == -1:
                 continue
             else:
                 await message.channel.send("Hi {}, I'm dad!".format(message.content[idx + len(s) + 1:]))
+                break
     except:
         print('poopoo')
 
